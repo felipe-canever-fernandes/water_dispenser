d02c534ac57d1f3d5a8876f7a3809e4ba801a156
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2023-12-02 20:19:22 -0300

    Implement quantity input

--- source/water_dispenser.v
+++ source/water_dispenser.v
@@ -3,23 +3,64 @@ module water_dispenser
 	clock,
 	reset,
 	
-	counter
+	switches,
+	
+	button_add,
+	button_ok,
+	button_cancel,
+	
+	total_time
 );
+		parameter SWITCH_COUNT = 10;
+		parameter MAXIMUM_DIGIT_COUNT = 4;
+		
+
 		input wire clock;
 		input wire reset;
 		
-		output integer counter;
+		input wire [SWITCH_COUNT - 1 : 0] switches;
+		
+		input wire button_add;
+		input wire button_ok;
+		input wire button_cancel;
+
+		
+		output integer total_time;
+		
+		integer i;
+		reg has_added_digit;
+		integer added_digit_count;
+		
 		
 		initial begin
-			counter = 0;
+			total_time = 0;
+			added_digit_count = 0;
 		end
+		
 		
 		always @(posedge clock or posedge reset) begin
 			if (reset == 1) begin
-				counter = 0;
+				total_time = 0;
+				added_digit_count = 0;
 			end
 			else begin
-				counter = counter + 1;
+				// Se houver espaço no visor...
+				if (added_digit_count < MAXIMUM_DIGIT_COUNT) begin
+					has_added_digit = 0;
+				
+					// Passar por todos os interruptores.
+					for (i = 0; i < SWITCH_COUNT; i = i + 1) begin
+						// Se o interruptor estiver acionado...
+						if (!has_added_digit && switches[i] == 1) begin
+							// Adicionar o dígito correspondente ao total.
+							total_time = total_time * 10 + i;
+							
+							// Ignorar os interruptores mais significativos que este.
+							has_added_digit = 1;
+							added_digit_count = added_digit_count + 1;
+						end
+					end
+				end
 			end
 		end
 endmodule
--- source/water_dispenser_testbench.v
+++ source/water_dispenser_testbench.v
@@ -1,31 +1,102 @@
 `timescale 1ms/1us
 
 module water_dispenser_testbench;
-  reg clock;
-  reg reset;
+  parameter SWITCH_COUNT = 10;
   
-  wire integer counter;
+  reg clock;
+	reg reset;
+	
+	reg [SWITCH_COUNT - 1 : 0] switches;
+	
+	reg button_add;
+	reg button_ok;
+	reg button_cancel;
+  
+  wire integer total_time;
   
   water_dispenser dispenser
   (
     .clock(clock),
-    .reset(reset),
-    
-    .counter(counter)
+	  .reset(reset),
+	
+	  .switches(switches),
+	
+	  .button_add(button_add),
+	  .button_ok(button_ok),
+	  .button_cancel(button_cancel),
+	
+	  .total_time(total_time)
   );
   
   initial begin
     clock = 0;
-    reset = 0;
+	  reset = 0;
+	
+	  switches = 0;
+	
+	  button_add = 0;
+	  button_ok = 0;
+	  button_cancel = 0;
   end
   
   always #1 begin
     clock = ~clock;
   end
   
-  always #10 begin
-    reset = 1;
+  initial begin
+    #9;
+    
+    // 1
+    switches[1] = 1;
+    button_add = 1;
+    
     #1;
-    reset = 0;
+    
+    switches[1] = 0;
+    button_add = 0;
+    
+    #1;
+    
+    // 9
+    switches[9] = 1;
+    button_add = 1;
+    
+    #1;
+    
+    switches[9] = 0;
+    button_add = 0;
+    
+    #1;
+    
+    // 9
+    switches[9] = 1;
+    button_add = 1;
+    
+    #1;
+    
+    switches[9] = 0;
+    button_add = 0;
+    
+    #1;
+    
+    // 3
+    switches[3] = 1;
+    button_add = 1;
+    
+    #1;
+    
+    switches[3] = 0;
+    button_add = 0;
+    
+    #1;
+    
+    //
+    switches[5] = 1;
+    button_add = 1;
+    
+    #1;
+    
+    switches[5] = 0;
+    button_add = 0;
   end
 endmodule
