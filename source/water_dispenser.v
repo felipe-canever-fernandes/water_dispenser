b606c9f332e8dd831765e56e5410b4ef321d89b5
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2023-12-03 11:35:27 -0300

    Remove diacritics from comments

--- source/water_dispenser.v
+++ source/water_dispenser.v
@@ -21,14 +21,14 @@ module water_dispenser
 		input wire clock;
 		input wire reset;
 		
-		// Dígitos de 0 a 9
+		// Di­gitos de 0 a 9
 		input wire [SWITCH_COUNT - 1 : 0] switches;
 		
 		input wire button_add;
 		input wire button_ok;
 		input wire button_cancel;
 
-		// A quantidade de água em mL inserida
+		// A quantidade de agua em mL inserida
 		output integer total_amount;
 		
 		integer i;
@@ -48,7 +48,7 @@ module water_dispenser
 				added_digit_count = 0;
 			end
 			else begin
-				// Se houver espaço no visor...
+				// Se houver espaco no visor...
 				if (added_digit_count < MAXIMUM_DIGIT_COUNT) begin
 					has_added_digit = 0;
 				
@@ -56,12 +56,12 @@ module water_dispenser
 					for (i = 0; i < SWITCH_COUNT; i = i + 1) begin
 						// Se o interruptor estiver acionado...
 						if (!has_added_digit && switches[i] == 1) begin
-							// Adicionar o dígito correspondente ao total.
+							// Adicionar o di­gito correspondente ao total.
 							total_amount = total_amount * 10 + i;
 							
 							// Ignorar os interruptores mais significativos que este.
 							has_added_digit = 1;
-							// Registrar que mais um dígito foi inserido.
+							// Registrar que mais um di­gito foi inserido.
 							added_digit_count = added_digit_count + 1;
 						end
 					end
